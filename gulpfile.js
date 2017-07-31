b7f608a90f26e1409778b6206477b9e4787672e6
Su7ech <pctweaker@gmail.com>
2017-07-30 20:37:57 -0700

    Modified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,30 +6,48 @@ var imagemin = require("gulp-imagemin");
 var pug = require("gulp-pug");
 var prefixer = require("gulp-autoprefixer");
 var browserSync = require("browser-sync").create();
-
-gulp.task('server', function() {
-  browserSync.init({
-    server: {
-      baseDir: "src/"
-    },
-    notify: false
-  });
-});
+var newer = require("gulp-newer");
 
 gulp.task('sass', function() {
   return gulp.src("src/scss/**/*.+(scss|sass)")
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write())
+    .pipe(prefixer({
+      browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+    }))
     .pipe(gulp.dest("src/css"))
     .pipe(browserSync.stream());
 });
 
-gulp.task('images', function() {
+gulp.task('imagemin', function() {
   return gulp.src("src/img/*")
     .pipe(newer())
     .pipe(cache(imagemin({
       interlaced: true
     })))
     .pipe(gulp.dest("dist/img"));
-})
+});
+
+gulp.task('views', function() {
+  return gulp.src("src/views/!(_)*.pug")
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest("src/"))
+    .pipe(browserSync.stream());
+});
+  
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "src/"
+    },
+    notify: false
+  });
+
+  gulp.watch("src/scss/**/*.+(scss|sass)", ['sass']);
+  gulp.watch("src/views/**/*.pug", ['views']);
+  gulp.watch("src/js/**/*.js").on('change', browserSync.reload);
+  gulp.watch("src/**/*.html").on('change', browserSync.reload)
+});
